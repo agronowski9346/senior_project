14ddddc263c0efd3a9ef40b47f7ed64501e8632b
agronowski9346 <agronowski9346@gmail.com>
2018-11-21 15:07:31 -0500

    import get objects or 404

--- fileUploader/views.py
+++ fileUploader/views.py
@@ -11,6 +11,7 @@ from fileCloud.serializers import UserSerializer
 from fileUploader.models import User
 from django.contrib.auth.hashers import make_password
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 #from fileUploader.models import Product   ;sample
 # Create your views here.
  
